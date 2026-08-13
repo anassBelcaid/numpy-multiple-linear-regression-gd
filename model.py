dce2e5e752b5f0e25988b30bfa78e50ef8eff00a
anass <anass.belcaid@gmail.com>
2026-08-13 19:11:24 +0100

    Step 5: add_bias_column

--- model.py
+++ model.py
@@ -62,8 +62,13 @@ def standardize_features(X, mean, std):
     # TODO: Apply z-score normalization using precomputed training mean and std.
     return (X - mean) / std
 
-# Step 5 - add_bias_column (not yet solved)
-# TODO: implement
+# Step 5 - add_bias_column
+def add_bias_column(X):
+    # TODO: Prepend a column of ones to feature matrix X
+    n = X.shape[0]
+    ones = np.ones((n, 1))
+    X = np.concatenate((ones, X), axis=1)
+    return X
 
 # Step 6 - prepare_design_matrix (not yet solved)
 # TODO: implement
